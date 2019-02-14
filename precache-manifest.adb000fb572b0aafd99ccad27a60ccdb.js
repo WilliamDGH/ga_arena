eae1f4e9e79b01f9b4843d07ca9f9b8125ef8dda
Guanhua <william.ting.1116@gmail.com>
2019-02-15 03:18:55 +1100

    Updates

---
+++ precache-manifest.adb000fb572b0aafd99ccad27a60ccdb.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "fa5a6cc3baa7e6297819",
+    "url": "/ga_arena/static/css/main.70da9677.chunk.css"
+  },
+  {
+    "revision": "fa5a6cc3baa7e6297819",
+    "url": "/ga_arena/static/js/main.fa5a6cc3.chunk.js"
+  },
+  {
+    "revision": "a90419cdd5ff85b4f0ef",
+    "url": "/ga_arena/static/js/runtime~main.a90419cd.js"
+  },
+  {
+    "revision": "0705f818368c3afd80a0",
+    "url": "/ga_arena/static/js/2.0705f818.chunk.js"
+  },
+  {
+    "revision": "d1106a8f8ca04dd68cb9c4b5e1c42c5e",
+    "url": "/ga_arena/static/media/tic-tac.d1106a8f.png"
+  },
+  {
+    "revision": "8c530fc08d9e9f0e52ef88dae8bc1dc9",
+    "url": "/ga_arena/static/media/gomoku1.8c530fc0.png"
+  },
+  {
+    "revision": "7db97312febf3884900242c5eba064f0",
+    "url": "/ga_arena/static/media/coming1.7db97312.png"
+  },
+  {
+    "revision": "68b329da9893e34099c7d8ad5cb9c940",
+    "url": "/ga_arena/static/media/logo.68b329da.svg"
+  },
+  {
+    "revision": "46c946a31e396493c02c1a9317e4b262",
+    "url": "/ga_arena/index.html"
+  }
+];
